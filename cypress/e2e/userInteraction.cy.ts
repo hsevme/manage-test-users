43dca041546b19879640799bfd59893aa52e13b3
hsevme <>
2024-04-26 23:51:06 +0200

    Strip spec file off tests

--- cypress/e2e/userInteraction.cy.ts
+++ cypress/e2e/userInteraction.cy.ts
@@ -22,31 +22,6 @@ describe("register and login user from the homepage", () => {
 
   it.only("does nothing", () => {});
 
-  it("can login a registered test user", () => {
-    cy.loginUser(/* change if you want to log in another user*/ happyHackor);
-  });
-
-  it("can log in and log out a given user", () => {
-    cy.loginUser(/* change if you want to log in another user*/ happyHackor);
-    cy.logoutUser();
-  });
-
-  it("can create all given test users", () => {
-    users.forEach((user) => {
-      cy.registerNewUser(user);
-      cy.logoutUser();
-      cy.visit(website.baseUrl);
-    });
-  });
-
-  it("can delete all given test users", () => {
-    users.forEach((user) => {
-      cy.loginUser(user);
-      cy.deleteUser();
-      cy.visit(website.baseUrl);
-    });
-  });
-
   after(() => {
     cy.visit(website.baseUrl);
     users.forEach((user) => {
