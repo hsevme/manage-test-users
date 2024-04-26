f7e292d3b915283440a39779c881100b589485af
hsevme <>
2024-04-26 18:37:32 +0200

    Update spec

--- cypress/e2e/userInteraction.cy.ts
+++ cypress/e2e/userInteraction.cy.ts
@@ -5,20 +5,54 @@ const happyHippo = users[0];
 const happyHackor = users[1];
 
 describe("register and login user from the homepage", () => {
-  // cy.session("testUser", () => {});
+  before(() => {
+    cy.visit(website.baseUrl);
+    cy.dismissConsentLayer(true);
+    users.forEach((user) => {
+      cy.registerNewUser(user);
+      cy.visit(website.baseUrl);
+      cy.logoutUser();
+      cy.visit(website.baseUrl);
+    });
+  });
 
   beforeEach(() => {
     cy.visit(website.baseUrl);
-    cy.dismissConsentLayer(true);
   });
 
   it.only("does nothing", () => {});
 
-  it("can register a new user", () => {
-    cy.registerNewUser(happyHackor);
+  it("can login a registered test user", () => {
+    cy.loginUser(/* change if you want to log in another user*/ happyHackor);
   });
 
-  it("can login a user", () => {
-    cy.loginUser(happyHackor);
+  it("can log in and log out a given user", () => {
+    cy.loginUser(/* change if you want to log in another user*/ happyHackor);
+    cy.logoutUser();
+  });
+
+  it("can create all given test users", () => {
+    users.forEach((user) => {
+      cy.registerNewUser(user);
+      cy.logoutUser();
+      cy.visit(website.baseUrl);
+    });
+  });
+
+  it("can delete all given test users", () => {
+    users.forEach((user) => {
+      cy.loginUser(user);
+      cy.deleteUser();
+      cy.visit(website.baseUrl);
+    });
+  });
+
+  after(() => {
+    cy.visit(website.baseUrl);
+    users.forEach((user) => {
+      cy.loginUser(user);
+      cy.deleteUser();
+      cy.visit(website.baseUrl);
+    });
   });
 });
