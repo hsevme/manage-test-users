9b31056c5efab81f1116679f26c18473265353e5
hsevme <>
2024-04-27 03:11:48 +0200

    Add config options

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,10 @@
+import * as website from "@fixtures/websites.json";
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: website.baseUrl,
+    viewportWidth: 1500,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
